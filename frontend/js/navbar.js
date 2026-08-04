96b5155059d491cb39c10208a075214022e71b30
Erhel czc <erhelito@yahoo.fr>
2026-08-04 11:52:05 +0200

    formatting

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -2,6 +2,6 @@ const burger = document.querySelector('.burger');
 const nav = document.querySelector('.navbar nav');
 
 burger.addEventListener('click', () => {
-  const open = nav.classList.toggle('open');
-  burger.setAttribute('aria-expanded', open);
+    const open = nav.classList.toggle('open');
+    burger.setAttribute('aria-expanded', open);
 });
